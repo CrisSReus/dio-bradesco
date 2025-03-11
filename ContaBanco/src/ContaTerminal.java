a2be6e45717946d45b5b9b128694ae7edf161139
Cristina <cristina.s.reus@gmail.com>
2025-03-10 23:42:59 -0300

    pronto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -7,29 +7,29 @@ public class ContaTerminal {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        //conhecer e importar classe scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (//conhecer e importar classe scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            //configurar dados
+            int conta;
+            int agencia;
+            String nomeCliente;
+            float saldo;
 
-        //configurar dados
-        int conta;
-        int agencia;
-        String nomeCliente;
-        float saldo;
+            //obter dados pelo scanner
+            System.out.println("Por favor, digite o número da Agência !");
+            agencia = scanner.nextInt(); 
+            
+            System.out.println("Por favor, digite o número da Conta !");
+            conta = scanner.nextInt();
 
-        //obter dados pelo scanner
-        System.out.println("Por favor, digite o número da Agência !");
-        agencia = scanner.nextInt(); 
-        
-        System.out.println("Por favor, digite o número da Conta !");
-        conta = scanner.nextInt();
+            System.out.println("Por favor, digite o nome do Cliente !");
+            nomeCliente = scanner.next();
 
-        System.out.println("Por favor, digite o nome do Cliente !");
-        nomeCliente = scanner.next();
+            System.out.println("Por favor, digite o Saldo !");
+            saldo = scanner.nextFloat();
 
-        System.out.println("Por favor, digite o Saldo !");
-        saldo = scanner.nextFloat();
-
-        // retornar mensagem para o cliente
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+            // retornar mensagem para o cliente
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+        }
     }
 }
