b5c4080c6cfb47eecddbf8e8eb386fa09780a428
Cristina <cristina.s.reus@gmail.com>
2025-03-13 20:37:51 -0300

    novo

--- ControleCandidatos/src/candidatura/ProcessoSeletivo.java
+++ ControleCandidatos/src/candidatura/ProcessoSeletivo.java
@@ -4,6 +4,9 @@ public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo seletivo");
     }
+    static void selecaoDeCandidatos(){
+        String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+    }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
